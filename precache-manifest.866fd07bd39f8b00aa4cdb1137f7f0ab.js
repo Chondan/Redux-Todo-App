1a830f55169b92e99141570b7d1c50aa675da544
Chondan Susuwan <chondansusuwan@outlook.coom>
2020-11-07 23:16:49 +0700

    uploaded build folder

---
+++ precache-manifest.866fd07bd39f8b00aa4cdb1137f7f0ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53eccd83ed6ead45f8553a83838ca4a",
+    "url": "/Redux-Todo-App/index.html"
+  },
+  {
+    "revision": "a0f28fdb418db06354d7",
+    "url": "/Redux-Todo-App/static/css/main.93017ba5.chunk.css"
+  },
+  {
+    "revision": "8934ab6c46fc51d1b670",
+    "url": "/Redux-Todo-App/static/js/2.8837d97b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Redux-Todo-App/static/js/2.8837d97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f28fdb418db06354d7",
+    "url": "/Redux-Todo-App/static/js/main.6fd016ea.chunk.js"
+  },
+  {
+    "revision": "12c268690310e32dd6fb",
+    "url": "/Redux-Todo-App/static/js/runtime-main.f6663967.js"
+  }
+]);
